f1d46cc822b4a01a531422fc9b46d575f0b28cc9
Alexey Inshakov <alexeyinshakov992@gmail.com>
2024-03-09 18:52:30 +0300

    v1

---
+++ src/js/app.js
@@ -0,0 +1,10 @@
+import * as flsFunctions from "./modules/functions.js";
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+flsFunctions.isWebp();
+
